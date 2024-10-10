555fdecef981d75f73cc674854060366bc5939c2
Mohamed_S._Alkindi <msbakng@gmail.com>
2024-10-10 09:22:32 +0000

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,134 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(StoreApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class StoreApp extends StatefulWidget {
+  const StoreApp({Key? key}) : super(key: key);
+
+  @override
+  State<StoreApp> createState() => _StoreAppState();
+}
+
+class _StoreAppState extends State<StoreApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Sign in"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Icon(
+                Icons.store,
+                size: 100,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter username",
+                  hintStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,
+                  ),
+                  labelText: "Username",
+                  labelStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),
+                  icon: const Icon(
+                    Icons.person,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(color: Colors.black, width: 5),
+                  ),
+                ),
+                keyboardType: TextInputType.text,
+              ),
+              const SizedBox(height: 40),
+              // TODO: Put the icon inside the border.
+              // TODO: Add functionality to hide and view password.
+              // TODO: HW Create a new project that has appbar login, Icon 2 textboxes.
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter password",
+                  hintStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,
+                  ),
+                  labelText: "Password",
+                  labelStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),
+                  // prefixIcon:
+                  icon: const Icon(
+                    Icons.lock,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(color: Colors.black, width: 5),
+                  ),
+                ),
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter Email",
+                  hintStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,
+                  ),
+                  labelText: "Email",
+                  labelStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),
+                  icon: const Icon(
+                    Icons.email,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(color: Colors.black, width: 5),
+                  ),
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter phone number",
+                  hintStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,
+                  ),
+                  labelText: "Phone number",
+                  labelStyle: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                  ),
+                  icon: const Icon(
+                    Icons.phone,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    borderSide: BorderSide(color: Colors.black, width: 5),
+                  ),
+                ),
+                keyboardType: TextInputType.phone,
+              ),
+            ],
+          ),
         ),
       ),
     );
